1683ba70736954527154784179a6a2913a3abbad
tkfjysft <tkfjysft.2013.frgn@gmail.com>
2026-07-12 01:10:34 +0900

    feat:SEO対策

---
+++ src/data/site.ts
@@ -0,0 +1,20 @@
+
+export const NAV_ITEMS = [
+  { text: 'トップ', url: '/', icon: 'Sun' },
+  { text: '鑑定メニュー', url: '#service', icon: 'Sparkles' }, 
+  { text: 'お客様の声', url: '#voice', icon: 'MessageCircle' },
+  { text: 'よくあるご質問', url: '#faq', icon: 'HelpCircle' },
+  { text: 'プロフィール', url: '#profile', icon: 'User' },
+  { text: 'ご予約/お問合せ', url: '/contact', icon: 'Mail' },
+];
+
+export const SITE_TITLE = "Spiritual Town"; 
+export const DESCRIPTION = "占い、癒やし、瞑想、カウンセリング。あなたの悩みや願いに合わせて、理想のスピリチュアル体験が見つかるポータルサイトです。心と体のバランスを整え、明日をもっと前向きに過ごすためのヒントをここから見つけませんか？"; 
+export const OWNER_NAME = "";
+export const OWNER_FULLNAME = "";
+export const OWNER_NAME_EN = "";
+export const POST_CODE = "*******";
+export const ADDRESS = "東京都千代田区丸の内1丁目9-1";
+export const TEL = "0000000000";
+export const FAX = "00\u200c0000\u200c0000";
+export const GOOGLE_MAP_URL = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.8280303808788!2d139.76493611234!3d35.6812361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfbd89f700b%3A0x44c8309623e03!2z5p2x5Lqs6aeF!5e0!3m2!1sja!2sjp!4v1710000000000!5m2!1sja!2sjp";
