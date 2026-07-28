dcf0ec27d61902c742d06f602d332b42a6b50c60
tkfjysft <tkfjysft.2013.frgn@gmail.com>
2026-07-29 00:09:29 +0900

    feat:サチコ2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  site: 'https://spi-town.com', 
 });
